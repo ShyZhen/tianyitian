1282ae7df6d0c35abe039a4462d6eeedde8967ae
zhenhuaixiu <zhenhuaixiu@sznews.com>
2022-01-07 16:21:28 +0800

    使用后台安全校验

--- apis/api.js
+++ apis/api.js
@@ -17,7 +17,24 @@ function uniUploadImage(filePath, type) {
     })
 }
 
+function imageSecCheck(filePath) {
+    return new Promise((resolve, reject) => {
+        uni.uploadFile({
+            url: request.buildFullUrl('V1/image/sec-check/'),
+            filePath: filePath,
+            name: 'image',
+            header: request.buildHeader({}),
+            success: (uploadRes) => {
+                resolve(uploadRes)
+            },
+            fail: (failRes) => {
+                reject(failRes)
+            }
+        });
+    })
+}
+
 
 export {
-    uniUploadImage,
+    uniUploadImage, imageSecCheck
 }
