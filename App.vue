630554b8bdc0a2329eaa3444833de502d26c0bd9
zhenhuaixiu <zhenhuaixiu@sznews.com>
2022-01-07 15:06:11 +0800

    添加强制更新逻辑

--- App.vue
+++ App.vue
@@ -30,6 +30,38 @@
 		},
 		onLaunch: function() {
 			console.log('App Launch');
+
+			// 强制更新
+      const updateManager = wx.getUpdateManager()
+      updateManager.onCheckForUpdate(function (res) {
+        // 请求完新版本信息的回调
+        console.log("强制更新检查", res.hasUpdate)
+      })
+
+      updateManager.onUpdateReady(function () {
+        wx.showModal({
+          title: '更新提示',
+          content: '新版本已经准备好，是否重启应用？',
+          success(res) {
+            if (res.confirm) {
+              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              updateManager.applyUpdate()
+            }
+          }
+        })
+      })
+
+      updateManager.onUpdateFailed(function () {
+        wx.hideLoading();
+        wx.showModal({
+          title: '提示',
+          content: '检查到有新版本，但下载失败，请检查网络后重试',
+          showCancel: false
+        })
+      })
+
+
+      // 初始化云函数
       if (!wx.cloud) {
         uni.showToast({
           title: '请使用 2.2.3 或以上的基础库以使用云能力',
